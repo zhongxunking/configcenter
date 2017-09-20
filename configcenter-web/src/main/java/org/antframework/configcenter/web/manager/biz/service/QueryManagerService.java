1d6d96ffbdf8670187329957ce1d5078fbf3b9e0
zhongxunking <zhongxunking@163.com>
2017-09-20 22:00:51 +0800

    页面

--- configcenter-web/src/main/java/org/antframework/configcenter/web/manager/biz/service/QueryManagerService.java
+++ configcenter-web/src/main/java/org/antframework/configcenter/web/manager/biz/service/QueryManagerService.java
@@ -42,10 +42,10 @@ public class QueryManagerService {
     public Map<String, Object> buildSearchParams(QueryManagerOrder queryManagerOrder) {
         Map<String, Object> searchParams = new HashMap<>();
         if (queryManagerOrder.getCode() != null) {
-            searchParams.put("LIKE_code", queryManagerOrder.getCode());
+            searchParams.put("LIKE_code", "%" + queryManagerOrder.getCode() + "%");
         }
         if (queryManagerOrder.getName() != null) {
-            searchParams.put("LIKE_username", queryManagerOrder.getName());
+            searchParams.put("LIKE_name", "%" + queryManagerOrder.getName() + "%");
         }
         if (queryManagerOrder.getType() != null) {
             searchParams.put("EQ_type", queryManagerOrder.getType());
