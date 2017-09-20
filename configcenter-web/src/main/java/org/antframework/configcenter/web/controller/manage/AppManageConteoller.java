3e2c5871754e7287d36e7ad2d4b11cf6cc237324
zhongxunking <zhongxunking@163.com>
2017-09-20 19:41:57 +0800

    web权限

--- configcenter-web/src/main/java/org/antframework/configcenter/web/controller/manage/AppManageConteoller.java
+++ configcenter-web/src/main/java/org/antframework/configcenter/web/controller/manage/AppManageConteoller.java
@@ -8,13 +8,26 @@
  */
 package org.antframework.configcenter.web.controller.manage;
 
+import org.antframework.boot.bekit.AntBekitException;
+import org.antframework.common.util.facade.AbstractQueryResult;
+import org.antframework.common.util.facade.Status;
+import org.antframework.configcenter.facade.api.ConfigService;
 import org.antframework.configcenter.facade.api.manage.AppManageService;
+import org.antframework.configcenter.facade.info.AppInfo;
+import org.antframework.configcenter.facade.order.FindAppOrder;
 import org.antframework.configcenter.facade.order.manage.AddOrModifyAppOrder;
 import org.antframework.configcenter.facade.order.manage.DeleteAppOrder;
 import org.antframework.configcenter.facade.order.manage.QueryAppOrder;
+import org.antframework.configcenter.facade.result.FindAppResult;
 import org.antframework.configcenter.facade.result.manage.AddOrModifyAppResult;
 import org.antframework.configcenter.facade.result.manage.DeleteAppResult;
-import org.antframework.configcenter.facade.result.manage.QueryAppResult;
+import org.antframework.configcenter.web.common.SessionAccessor;
+import org.antframework.configcenter.web.manager.facade.api.ManagerAppManageService;
+import org.antframework.configcenter.web.manager.facade.enums.ManagerType;
+import org.antframework.configcenter.web.manager.facade.info.ManagerAppInfo;
+import org.antframework.configcenter.web.manager.facade.info.ManagerInfo;
+import org.antframework.configcenter.web.manager.facade.order.QueryManagedAppOrder;
+import org.antframework.configcenter.web.manager.facade.result.QueryManagedAppResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +40,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppManageConteoller extends AbstractController {
     @Autowired
     private AppManageService appManageService;
+    @Autowired
+    private ManagerAppManageService managerAppManageService;
+    @Autowired
+    private ConfigService configService;
 
     /**
      * 添加或修改应用
@@ -66,13 +83,37 @@ public class AppManageConteoller extends AbstractController {
      * @param appCode  应用编码（可选，有值会进行模糊查询）
      */
     @RequestMapping("/queryApp")
-    public QueryAppResult queryApp(int pageNo, int pageSize, String appCode) {
+    public AbstractQueryResult<AppInfo> queryApp(int pageNo, int pageSize, String appCode) {
         assertLogined();
-        QueryAppOrder order = new QueryAppOrder();
-        order.setPageNo(pageNo);
-        order.setPageSize(pageSize);
-        order.setAppCode(appCode);
-
-        return appManageService.queryApp(order);
+        ManagerInfo managerInfo = SessionAccessor.getManagerInfo();
+        if (managerInfo.getType() == ManagerType.ADMIN) {
+            QueryAppOrder order = new QueryAppOrder();
+            order.setPageNo(pageNo);
+            order.setPageSize(pageSize);
+            order.setAppCode(appCode);
+            return appManageService.queryApp(order);
+        } else {
+            QueryManagedAppOrder order = new QueryManagedAppOrder();
+            order.setPageNo(pageNo);
+            order.setPageSize(pageSize);
+            order.setManagerCode(managerInfo.getCode());
+            order.setAppCode(appCode);
+            QueryManagedAppResult queryManagedAppResult = managerAppManageService.queryManagedApp(order);
+            if (!queryManagedAppResult.isSuccess()) {
+                throw new AntBekitException(Status.FAIL, queryManagedAppResult.getCode(), queryManagedAppResult.getMessage());
+            }
+            AbstractQueryResult<AppInfo> result = new AbstractQueryResult<AppInfo>() {
+            };
+            for (ManagerAppInfo info : queryManagedAppResult.getInfos()) {
+                FindAppOrder findAppOrder = new FindAppOrder();
+                findAppOrder.setAppCode(info.getAppCode());
+                FindAppResult findAppResult = configService.findApp(findAppOrder);
+                if (!findAppResult.isSuccess()) {
+                    throw new AntBekitException(Status.FAIL, findAppResult.getCode(), findAppResult.getMessage());
+                }
+                result.addInfo(findAppResult.getAppInfo());
+            }
+            return result;
+        }
     }
 }
