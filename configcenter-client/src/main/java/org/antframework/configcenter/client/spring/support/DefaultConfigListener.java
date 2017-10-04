a6b106443bafe8e17a2c446ec34b2613ad7d3dfc
zhongxunking <zhongxunking@163.com>
2017-10-04 11:25:51 +0800

    客户端:spring.support

--- configcenter-client/src/main/java/org/antframework/configcenter/client/spring/support/DefaultConfigListener.java
+++ configcenter-client/src/main/java/org/antframework/configcenter/client/spring/support/DefaultConfigListener.java
@@ -6,7 +6,7 @@
  * 修订记录:
  * @author 钟勋 2017-09-25 22:16 创建
  */
-package org.antframework.configcenter.client.spring.boot;
+package org.antframework.configcenter.client.spring.support;
 
 import org.antframework.configcenter.client.ConfigListener;
 import org.antframework.configcenter.client.core.ModifiedProperty;
--- configcenter-client/src/main/java/org/antframework/configcenter/client/spring/support/ListenConfigModifiedTrigger.java
+++ configcenter-client/src/main/java/org/antframework/configcenter/client/spring/support/ListenConfigModifiedTrigger.java
@@ -6,7 +6,7 @@
  * 修订记录:
  * @author 钟勋 2017-09-25 22:21 创建
  */
-package org.antframework.configcenter.client.spring.boot;
+package org.antframework.configcenter.client.spring.support;
 
 import org.antframework.configcenter.client.ConfigContext;
 import org.antframework.configcenter.client.ConfigListener;
