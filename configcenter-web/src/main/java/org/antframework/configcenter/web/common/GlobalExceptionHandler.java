e2c419c02574c83d2e3821016db5e63a62b2ad1f
zhongxunking <zhongxunking@163.com>
2018-02-06 21:36:24 +0800

    优化web层异常捕获

--- configcenter-web/src/main/java/org/antframework/configcenter/web/common/GlobalExceptionHandler.java
+++ configcenter-web/src/main/java/org/antframework/configcenter/web/common/GlobalExceptionHandler.java
@@ -6,9 +6,12 @@
  * 修订记录:
  * @author 钟勋 2017-09-14 16:41 创建
  */
-package org.antframework.configcenter.web.controller;
+package org.antframework.configcenter.web.common;
 
-import org.antframework.common.util.facade.*;
+import org.antframework.common.util.facade.BizException;
+import org.antframework.common.util.facade.CommonResultCode;
+import org.antframework.common.util.facade.EmptyResult;
+import org.antframework.common.util.facade.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,8 +24,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    // 处理BizException
     @ExceptionHandler(BizException.class)
-    public AbstractResult handleBizException(BizException e) {
+    public EmptyResult handleBizException(BizException e) {
         EmptyResult result = new EmptyResult();
         result.setStatus(e.getStatus());
         result.setCode(e.getCode());
@@ -31,10 +35,10 @@ public class GlobalExceptionHandler {
         return result;
     }
 
+    // 处理Exception
     @ExceptionHandler(Exception.class)
-    public AbstractResult handleException(Exception e) {
-        logger.error("web层发生异常：", e);
-
+    public EmptyResult handleException(Exception e) {
+        logger.error("web层捕获到未知异常：", e);
         EmptyResult result = new EmptyResult();
         result.setStatus(Status.PROCESSING);
         result.setCode(CommonResultCode.UNKNOWN_ERROR.getCode());
