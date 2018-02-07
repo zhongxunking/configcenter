565f41ea8f9f5fe1c1507bfe60735c1f0cb81602
zhongxunking <zhongxunking@163.com>
2018-02-07 18:40:38 +0800

    完善页面

--- configcenter-web/src/main/resources/static/common/commonManager.js
+++ configcenter-web/src/main/resources/static/common/commonManager.js
@@ -1,22 +1,21 @@
-var managerInfo = null;
+var manager = null;
 $(document).ready(function () {
-    managerInfo = getLoginedManager();
+    manager = getCurrentManager();
+    if (manager == null) {
+        alert("未登录，或登录超时，请重新登陆");
+        window.top.location.href = "login.html";
+    }
 });
 
-function getLoginedManager() {
-    var managerInfo;
+function getCurrentManager() {
+    var manager = null;
     $.ajax({
-        url: "/manage/managerLogin/getLoginedManager",
+        url: "../manager/main/current",
         data: {},
         async: false,
         success: function (ajaxResult) {
             if (ajaxResult.success) {
-                if (ajaxResult.managerInfo == null) {
-                    alert("未登录，或登录超时，请重新登陆");
-                    window.location.href = "login.html";
-                } else {
-                    managerInfo = ajaxResult.managerInfo;
-                }
+                manager = ajaxResult.manager;
             } else {
                 alert("失败，原因：" + ajaxResult.message);
             }
@@ -25,5 +24,5 @@ function getLoginedManager() {
             alert("请求服务失败");
         }
     });
-    return managerInfo;
+    return manager;
 }
