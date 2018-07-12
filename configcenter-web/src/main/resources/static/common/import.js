f168dd1ea9266a0cffa2c8c8fa4df2bc290e6977
zhongxunking <zhongxunking@163.com>
2018-07-13 01:09:19 +0800

    main.html增加导航栏

--- configcenter-web/src/main/resources/static/common/import.js
+++ configcenter-web/src/main/resources/static/common/import.js
@@ -1,4 +1,5 @@
 document.write('<script src="../lib/vue.js"></script>');
+document.write('<script src="../lib/vue-router.js"></script>');
 document.write('<link rel="stylesheet" href="../lib/element-ui/element-ui.css">');
 document.write('<script src="../lib/element-ui/element-ui.js"></script>');
 document.write('<script src="../lib/axios.js"></script>');
