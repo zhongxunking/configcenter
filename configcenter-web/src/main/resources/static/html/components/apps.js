d217e3215b9424810289ff2334bf8dde43a83b13
zhongxunking <zhongxunking@163.com>
2018-07-23 23:35:17 +0800

    应用管理页面微调

--- configcenter-web/src/main/resources/static/html/components/apps.js
+++ configcenter-web/src/main/resources/static/html/components/apps.js
@@ -133,7 +133,7 @@ const appsComponent = {
                     Vue.set(app, 'editing', false);
                     Vue.set(app, 'editingMemo', null);
                     Vue.set(app, 'editingParent', null);
-                    Vue.set(app, 'savePopoverShowing', null);
+                    Vue.set(app, 'savePopoverShowing', false);
                     Vue.set(app, 'parentApp', null);
                     if (app.parent) {
                         theThis.doFindApp(app.parent, function (parentApp) {
