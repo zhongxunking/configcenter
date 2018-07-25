fd3e3a8c3aac29f2780788c8e099835c91f48c0d
zhongxunking <zhongxunking@163.com>
2018-07-26 02:46:25 +0800

    web增加查找应用继承的属性key

--- configcenter-web/src/main/java/org/antframework/configcenter/web/controller/manage/PropertyKeyManageController.java
+++ configcenter-web/src/main/java/org/antframework/configcenter/web/controller/manage/PropertyKeyManageController.java
@@ -8,17 +8,29 @@
  */
 package org.antframework.configcenter.web.controller.manage;
 
+import org.antframework.common.util.facade.AbstractResult;
+import org.antframework.common.util.facade.BizException;
 import org.antframework.common.util.facade.EmptyResult;
+import org.antframework.common.util.facade.Status;
+import org.antframework.configcenter.facade.api.AppService;
 import org.antframework.configcenter.facade.api.PropertyKeyService;
+import org.antframework.configcenter.facade.info.AppInfo;
+import org.antframework.configcenter.facade.info.PropertyKeyInfo;
 import org.antframework.configcenter.facade.order.AddOrModifyPropertyKeyOrder;
 import org.antframework.configcenter.facade.order.DeletePropertyKeyOrder;
 import org.antframework.configcenter.facade.order.FindAppPropertyKeysOrder;
+import org.antframework.configcenter.facade.order.FindInheritedAppsOrder;
 import org.antframework.configcenter.facade.result.FindAppPropertyKeysResult;
+import org.antframework.configcenter.facade.result.FindInheritedAppsResult;
 import org.antframework.configcenter.facade.vo.Scope;
 import org.antframework.manager.web.common.ManagerAssert;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 属性key管理controller
@@ -28,6 +40,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class PropertyKeyManageController {
     @Autowired
     private PropertyKeyService propertyKeyService;
+    @Autowired
+    private AppService appService;
 
     /**
      * 新增或修改属性key
@@ -66,16 +80,87 @@ public class PropertyKeyManageController {
     }
 
     /**
-     * 查找应用所有的属性key
+     * 查找应用继承的属性key（包含应用自己）
      *
-     * @param appId 应用id（必须）
+     * @param appId 应用id
      */
-    @RequestMapping("/findAppPropertyKeys")
-    public FindAppPropertyKeysResult findAppPropertyKeys(String appId) {
+    @RequestMapping("/findInheritedPropertyKeys")
+    public FindInheritedPropertyKeysResult findInheritedPropertyKeys(String appId) {
         ManagerAssert.adminOrHaveRelation(appId);
-        FindAppPropertyKeysOrder order = new FindAppPropertyKeysOrder();
+
+        FindInheritedPropertyKeysResult result = new FindInheritedPropertyKeysResult();
+        for (AppInfo app : getInheritedApp(appId)) {
+            result.addAppPropertyKeys(getAppPropertyKeys(app.getAppId(), appId));
+        }
+        return result;
+    }
+
+    // 获取应用继承的所有应用
+    private List<AppInfo> getInheritedApp(String appId) {
+        FindInheritedAppsOrder order = new FindInheritedAppsOrder();
         order.setAppId(appId);
 
-        return propertyKeyService.findAppPropertyKeys(order);
+        FindInheritedAppsResult result = appService.findInheritedApps(order);
+        if (!result.isSuccess()) {
+            throw new BizException(Status.FAIL, result.getCode(), result.getMessage());
+        }
+        return result.getInheritedApps();
+    }
+
+    // 获取应用的属性key
+    private FindInheritedPropertyKeysResult.AppPropertyKeys getAppPropertyKeys(String appId, String mainAppId) {
+        Scope minScope = Scope.PRIVATE;
+        if (!StringUtils.equals(appId, mainAppId)) {
+            minScope = Scope.PROTECTED;
+        }
+
+        FindAppPropertyKeysOrder order = new FindAppPropertyKeysOrder();
+        order.setAppId(appId);
+        order.setMinScope(minScope);
+
+        FindAppPropertyKeysResult result = propertyKeyService.findAppPropertyKeys(order);
+        if (!result.isSuccess()) {
+            throw new BizException(Status.FAIL, result.getCode(), result.getMessage());
+        }
+        return new FindInheritedPropertyKeysResult.AppPropertyKeys(appId, result.getPropertyKeys());
+    }
+
+    /**
+     * 查找应用继承的所有应用的属性key
+     */
+    public static class FindInheritedPropertyKeysResult extends AbstractResult {
+        // 由近及远继承的所用应用的属性key
+        private List<AppPropertyKeys> appPropertyKeyses = new ArrayList<>();
+
+        public void addAppPropertyKeys(AppPropertyKeys appPropertyKeys) {
+            appPropertyKeyses.add(appPropertyKeys);
+        }
+
+        public List<AppPropertyKeys> getAppPropertyKeyses() {
+            return appPropertyKeyses;
+        }
+
+        /**
+         * 应用属性key
+         */
+        public static class AppPropertyKeys {
+            // 应用id
+            private String appId;
+            // 属性key
+            private List<PropertyKeyInfo> propertyKeys;
+
+            public AppPropertyKeys(String appId, List<PropertyKeyInfo> propertyKeys) {
+                this.appId = appId;
+                this.propertyKeys = propertyKeys;
+            }
+
+            public String getAppId() {
+                return appId;
+            }
+
+            public List<PropertyKeyInfo> getPropertyKeys() {
+                return propertyKeys;
+            }
+        }
     }
 }
