bbf4f36ed014e6d670ec8207e5b5710fd48c173f
zhongxunking <zhongxunking@163.com>
2018-08-05 12:40:45 +0800

    新增环境、管理员图标

--- configcenter-web/src/main/resources/static/common/import.js
+++ configcenter-web/src/main/resources/static/common/import.js
@@ -1,7 +1,8 @@
 document.write('<script src="../lib/vue.js"></script>');
 document.write('<script src="../lib/vue-router.js"></script>');
-document.write('<link rel="stylesheet" href="../lib/element-ui/element-ui.css">');
+document.write('<link rel="stylesheet" type="text/css" href="../lib/element-ui/element-ui.css">');
 document.write('<script src="../lib/element-ui/element-ui.js"></script>');
 document.write('<script src="../lib/axios.js"></script>');
 document.write('<script src="../lib/qs.js"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="../lib/icon/iconfont.css">');
 document.write('<script src="../common/common.js"></script>');
