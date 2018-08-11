27db8bf209ae748f3ad55403833e4778bef3c66e
zhongxunking <zhongxunking@163.com>
2018-08-11 17:56:09 +0800

    删除controller同步数据到zk

--- configcenter-web/src/main/java/org/antframework/configcenter/web/controller/manage/RefreshController.java
+++ configcenter-web/src/main/java/org/antframework/configcenter/web/controller/manage/RefreshController.java
@@ -6,13 +6,12 @@
  * 修订记录:
  * @author 钟勋 2017-09-16 16:34 创建
  */
-package org.antframework.configcenter.web.controller;
+package org.antframework.configcenter.web.controller.manage;
 
-import org.antframework.common.util.facade.EmptyOrder;
 import org.antframework.common.util.facade.EmptyResult;
-import org.antframework.common.util.facade.FacadeUtils;
 import org.antframework.configcenter.facade.api.RefreshService;
 import org.antframework.configcenter.facade.order.TriggerClientsRefreshOrder;
+import org.antframework.manager.web.common.ManagerAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,28 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
  * 刷新controller
  */
 @RestController
-@RequestMapping("/refresh")
+@RequestMapping("/manage/refresh")
 public class RefreshController {
     @Autowired
     private RefreshService refreshService;
 
     /**
-     * 先同步所有应用和环境到zookeeper，然后触发对应客户端刷新配置
+     * 触发客户端刷新配置
      *
      * @param appId     应用id（不传表示刷新所有应用）
      * @param profileId 环境id（不传表示刷新所有环境）
      */
-    @RequestMapping("/refreshZkAndClients")
-    public EmptyResult refreshZkAndClients(String appId, String profileId) {
-        // 同步数据到zookeeper
-        EmptyResult result = refreshService.syncDataToZk(new EmptyOrder());
-        FacadeUtils.assertSuccess(result);
-        // 触发客户端刷新配置
+    @RequestMapping("/triggerClientsRefresh")
+    public EmptyResult triggerClientsRefresh(String appId, String profileId) {
+        if (appId == null) {
+            ManagerAssert.admin();
+        } else {
+            ManagerAssert.adminOrHaveRelation(appId);
+        }
         TriggerClientsRefreshOrder order = new TriggerClientsRefreshOrder();
         order.setAppId(appId);
         order.setProfileId(profileId);
-        result = refreshService.triggerClientsRefresh(order);
 
-        return result;
+        return refreshService.triggerClientsRefresh(order);
     }
 }
