341d70ddddb80f1d0a47686ee6413acfaf29f981
zhongxunking <zhongxunking@163.com>
2018-08-12 20:10:53 +0800

    新增刷新zookeeper定时器

--- configcenter-biz/src/main/java/org/antframework/configcenter/biz/BizConfiguration.java
+++ configcenter-biz/src/main/java/org/antframework/configcenter/biz/BizConfiguration.java
@@ -9,11 +9,15 @@
 package org.antframework.configcenter.biz;
 
 import org.antframework.common.util.zookeeper.ZkTemplate;
+import org.antframework.configcenter.biz.util.RefreshUtils;
 import org.antframework.configcenter.facade.vo.ZkConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * biz层配置
@@ -21,6 +25,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(ConfigcenterProperties.class)
 public class BizConfiguration {
+    // 刷新zookeeper任务的执行周期（单位：毫秒）
+    private static final long REFRESH_ZK_TASK_RERIOD = 5 * 60 * 1000;
+
     @Autowired
     private ConfigcenterProperties properties;
 
@@ -28,5 +35,21 @@ public class BizConfiguration {
     @Bean
     public ZkTemplate zkTemplate() {
         return ZkTemplate.create(properties.getZkUrls().toArray(new String[0]), ZkConstant.ZK_CONFIG_NAMESPACE);
+    }
+
+    // 刷新zookeeper的定时器
+    @Bean(destroyMethod = "cancel")
+    public Timer refreshZkTimer() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                RefreshUtils.refreshZk();
+            }
+        };
+
+        Timer timer = new Timer("Timer-refreshZk", true);
+        timer.schedule(task, REFRESH_ZK_TASK_RERIOD, REFRESH_ZK_TASK_RERIOD);
+
+        return timer;
     }
 }
