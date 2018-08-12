f0cc9307d6552dd35093dc9740ab809370650f74
zhongxunking <zhongxunking@163.com>
2018-08-12 20:23:51 +0800

    修复ConfigcenterProperties警告

--- configcenter-biz/src/main/java/org/antframework/configcenter/biz/ConfigcenterProperties.java
+++ configcenter-biz/src/main/java/org/antframework/configcenter/biz/ConfigcenterProperties.java
@@ -10,6 +10,7 @@ package org.antframework.configcenter.biz;
 
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Set;
 
@@ -17,6 +18,7 @@ import java.util.Set;
  * 配置中心属性
  */
 @ConfigurationProperties("configcenter")
+@Validated
 public class ConfigcenterProperties {
     /**
      * 必填：配置中心使用的zookeeper地址，存在多个zookeeper的话以“,”分隔（比如：192.168.0.1:2181,192.168.0.2:2181）
