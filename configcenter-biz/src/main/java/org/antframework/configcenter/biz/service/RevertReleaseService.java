23c0bddaf75f4b2f9c9adb65c8c25dd69a3b49a4
zhongxunking <zhongxunking@163.com>
2018-12-10 23:21:45 +0800

    回滚发布时，同时回滚配置value

--- configcenter-biz/src/main/java/org/antframework/configcenter/biz/service/RevertReleaseService.java
+++ configcenter-biz/src/main/java/org/antframework/configcenter/biz/service/RevertReleaseService.java
@@ -8,10 +8,8 @@
  */
 package org.antframework.configcenter.biz.service;
 
-import org.antframework.common.util.facade.BizException;
-import org.antframework.common.util.facade.CommonResultCode;
-import org.antframework.common.util.facade.EmptyResult;
-import org.antframework.common.util.facade.Status;
+import org.antframework.common.util.facade.*;
+import org.antframework.configcenter.biz.util.PropertyValueUtils;
 import org.antframework.configcenter.biz.util.RefreshUtils;
 import org.antframework.configcenter.dal.dao.AppDao;
 import org.antframework.configcenter.dal.dao.ProfileDao;
@@ -19,12 +17,16 @@ import org.antframework.configcenter.dal.dao.ReleaseDao;
 import org.antframework.configcenter.dal.entity.App;
 import org.antframework.configcenter.dal.entity.Profile;
 import org.antframework.configcenter.dal.entity.Release;
+import org.antframework.configcenter.facade.api.PropertyValueService;
+import org.antframework.configcenter.facade.order.AddOrModifyPropertyValueOrder;
 import org.antframework.configcenter.facade.order.RevertReleaseOrder;
+import org.antframework.configcenter.facade.vo.Property;
 import org.antframework.configcenter.facade.vo.ReleaseConstant;
 import org.bekit.service.annotation.service.Service;
 import org.bekit.service.annotation.service.ServiceAfter;
 import org.bekit.service.annotation.service.ServiceExecute;
 import org.bekit.service.engine.ServiceContext;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -38,6 +40,8 @@ public class RevertReleaseService {
     private ProfileDao profileDao;
     @Autowired
     private ReleaseDao releaseDao;
+    @Autowired
+    private PropertyValueService propertyValueService;
 
     @ServiceExecute
     public void execute(ServiceContext<RevertReleaseOrder, EmptyResult> context) {
@@ -51,14 +55,37 @@ public class RevertReleaseService {
         if (profile == null) {
             throw new BizException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("环境[%s]不存在", order.getProfileId()));
         }
+        Release release = null;
         if (order.getVersion() > ReleaseConstant.ORIGIN_VERSION) {
-            Release release = releaseDao.findLockByAppIdAndProfileIdAndVersion(order.getAppId(), order.getProfileId(), order.getVersion());
+            release = releaseDao.findLockByAppIdAndProfileIdAndVersion(order.getAppId(), order.getProfileId(), order.getVersion());
             if (release == null) {
                 throw new BizException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("回滚到的目标发布[appId=%s,profileId=%s,version=%d]不存在", order.getAppId(), order.getProfileId(), order.getVersion()));
             }
         }
+        // 回滚配置value
+        revertPropertyValues(order.getAppId(), order.getProfileId(), release);
         // 回滚发布
         releaseDao.deleteByAppIdAndProfileIdAndVersionGreaterThan(order.getAppId(), order.getProfileId(), order.getVersion());
+    }
+
+    // 回滚配置value
+    private void revertPropertyValues(String appId, String profileId, Release release) {
+        // 删除现有配置value
+        PropertyValueUtils.deleteAppProfilePropertyValues(appId, profileId);
+        // 判断是否需要重建配置value
+        if (release == null) {
+            return;
+        }
+        // 使用发布重建配置value
+        for (Property property : release.getProperties()) {
+            AddOrModifyPropertyValueOrder order = new AddOrModifyPropertyValueOrder();
+            BeanUtils.copyProperties(property, order);
+            order.setAppId(appId);
+            order.setProfileId(profileId);
+
+            EmptyResult result = propertyValueService.addOrModifyPropertyValue(order);
+            FacadeUtils.assertSuccess(result);
+        }
     }
 
     @ServiceAfter
