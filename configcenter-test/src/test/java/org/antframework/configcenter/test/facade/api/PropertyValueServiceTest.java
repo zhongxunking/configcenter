bc1c65b5946e6b31a9acfe92f212e292cbcfcaec
zhongxunking <zhongxunking@163.com>
2018-12-11 23:09:23 +0800

    发布服务单元测试

--- configcenter-test/src/test/java/org/antframework/configcenter/test/facade/api/PropertyValueServiceTest.java
+++ configcenter-test/src/test/java/org/antframework/configcenter/test/facade/api/PropertyValueServiceTest.java
@@ -11,8 +11,9 @@ package org.antframework.configcenter.test.facade.api;
 import org.antframework.common.util.facade.EmptyResult;
 import org.antframework.common.util.facade.Status;
 import org.antframework.configcenter.facade.api.PropertyValueService;
+import org.antframework.configcenter.facade.order.AddOrModifyPropertyValueOrder;
+import org.antframework.configcenter.facade.order.DeletePropertyValueOrder;
 import org.antframework.configcenter.facade.order.FindAppProfilePropertyValuesOrder;
-import org.antframework.configcenter.facade.order.SetPropertyValuesOrder;
 import org.antframework.configcenter.facade.result.FindAppProfilePropertyValuesResult;
 import org.antframework.configcenter.facade.vo.Scope;
 import org.antframework.configcenter.test.AbstractTest;
@@ -29,55 +30,36 @@ public class PropertyValueServiceTest extends AbstractTest {
     private PropertyValueService propertyValueService;
 
     @Test
-    public void testSetPropertyValue() {
-        // 设置offline环境value
+    public void testAddOrModifyPropertyValue() {
+        String[] appIds = new String[]{"common", "core-domain", "account", "customer"};
+        String[] profileIds = new String[]{"offline", "dev"};
+        for (String appId : appIds) {
+            for (String profileId : profileIds) {
+                for (Scope scope : Scope.values()) {
+                    AddOrModifyPropertyValueOrder order = new AddOrModifyPropertyValueOrder();
+                    order.setAppId(appId);
+                    order.setKey(String.format("%s-%s-key1", appId, scope.name().toLowerCase()));
+                    order.setProfileId(profileId);
+                    order.setValue(String.format("%s-%s-value1-%s", appId, scope.name().toLowerCase(), profileId));
+                    order.setScope(scope);
+
+                    EmptyResult result = propertyValueService.addOrModifyPropertyValue(order);
+                    checkResult(result, Status.SUCCESS);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testDeletePropertyValue() {
         String[] appIds = new String[]{"common", "core-domain", "account", "customer"};
         for (String appId : appIds) {
-            SetPropertyValuesOrder order = new SetPropertyValuesOrder();
+            DeletePropertyValueOrder order = new DeletePropertyValueOrder();
             order.setAppId(appId);
-            order.setProfileId("offline");
-
-            for (Scope scope : Scope.values()) {
-                SetPropertyValuesOrder.KeyValue keyValue = new SetPropertyValuesOrder.KeyValue();
-                keyValue.setKey(String.format("%s-%s-key1", appId, scope.name().toLowerCase()));
-                keyValue.setValue(String.format("%s-%s-value1-%s", appId, scope.name().toLowerCase(), "offline"));
-                order.addKeyValue(keyValue);
-            }
-
-            EmptyResult result = propertyValueService.setPropertyValues(order);
-            checkResult(result, Status.SUCCESS);
-        }
-        // 删除value
-        for (String appId : appIds) {
-            SetPropertyValuesOrder order = new SetPropertyValuesOrder();
-            order.setAppId(appId);
-            order.setProfileId("offline");
-
-            SetPropertyValuesOrder.KeyValue keyValue = new SetPropertyValuesOrder.KeyValue();
-            keyValue.setKey(String.format("%s-%s-key1", appId, "public"));
-            keyValue.setValue(null);
-            order.addKeyValue(keyValue);
-
-            EmptyResult result = propertyValueService.setPropertyValues(order);
-            checkResult(result, Status.SUCCESS);
-        }
-        // 设置dev环境value
-        for (String appId : appIds) {
-            SetPropertyValuesOrder order = new SetPropertyValuesOrder();
-            order.setAppId(appId);
+            order.setKey(String.format("%s-%s-key1", appId, Scope.PRIVATE.name().toLowerCase()));
             order.setProfileId("dev");
 
-            for (Scope scope : Scope.values()) {
-                if (scope == Scope.PRIVATE) {
-                    continue;
-                }
-                SetPropertyValuesOrder.KeyValue keyValue = new SetPropertyValuesOrder.KeyValue();
-                keyValue.setKey(String.format("%s-%s-key1", appId, scope.name().toLowerCase()));
-                keyValue.setValue(String.format("%s-%s-value1-%s", appId, scope.name().toLowerCase(), "dev"));
-                order.addKeyValue(keyValue);
-            }
-
-            EmptyResult result = propertyValueService.setPropertyValues(order);
+            EmptyResult result = propertyValueService.deletePropertyValue(order);
             checkResult(result, Status.SUCCESS);
         }
     }
@@ -87,6 +69,7 @@ public class PropertyValueServiceTest extends AbstractTest {
         FindAppProfilePropertyValuesOrder order = new FindAppProfilePropertyValuesOrder();
         order.setAppId("customer");
         order.setProfileId("dev");
+        order.setMinScope(Scope.PRIVATE);
 
         FindAppProfilePropertyValuesResult result = propertyValueService.findAppProfilePropertyValues(order);
         checkResult(result, Status.SUCCESS);
---
+++ configcenter-test/src/test/java/org/antframework/configcenter/test/facade/api/ReleaseServiceTest.java
@@ -0,0 +1,104 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2018-12-11 22:55 创建
+ */
+package org.antframework.configcenter.test.facade.api;
+
+import org.antframework.common.util.facade.EmptyResult;
+import org.antframework.common.util.facade.Status;
+import org.antframework.configcenter.facade.api.ReleaseService;
+import org.antframework.configcenter.facade.order.AddReleaseOrder;
+import org.antframework.configcenter.facade.order.FindCurrentReleaseOrder;
+import org.antframework.configcenter.facade.order.QueryReleasesOrder;
+import org.antframework.configcenter.facade.order.RevertReleaseOrder;
+import org.antframework.configcenter.facade.result.AddReleaseResult;
+import org.antframework.configcenter.facade.result.FindCurrentReleaseResult;
+import org.antframework.configcenter.facade.result.QueryReleasesResult;
+import org.antframework.configcenter.facade.vo.ReleaseConstant;
+import org.antframework.configcenter.test.AbstractTest;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 发布服务单元测试
+ */
+@Ignore
+public class ReleaseServiceTest extends AbstractTest {
+    @Autowired
+    private ReleaseService releaseService;
+
+    @Test
+    private void testAddRelease() {
+        String[] appIds = new String[]{"common", "core-domain", "account", "customer"};
+        String[] profileIds = new String[]{"offline", "dev"};
+        for (String appId : appIds) {
+            for (String profileId : profileIds) {
+                AddReleaseOrder order = new AddReleaseOrder();
+                order.setAppId(appId);
+                order.setProfileId(profileId);
+                order.setMemo(String.format("测试-%s-%s", appId, profileId));
+
+                AddReleaseResult result = releaseService.addRelease(order);
+                checkResult(result, Status.SUCCESS);
+                Assert.assertNotNull(result.getRelease());
+            }
+        }
+    }
+
+    @Test
+    private void testRevertRelease() {
+        String[] appIds = new String[]{"common", "core-domain", "account", "customer"};
+        String[] profileIds = new String[]{"offline", "dev"};
+        for (String appId : appIds) {
+            for (String profileId : profileIds) {
+                RevertReleaseOrder order = new RevertReleaseOrder();
+                order.setAppId(appId);
+                order.setProfileId(profileId);
+                order.setVersion(ReleaseConstant.ORIGIN_VERSION);
+
+                EmptyResult result = releaseService.revertRelease(order);
+                checkResult(result, Status.SUCCESS);
+            }
+        }
+    }
+
+    @Test
+    private void testFindCurrentRelease() {
+        String[] appIds = new String[]{"common", "core-domain", "account", "customer"};
+        String[] profileIds = new String[]{"offline", "dev"};
+        for (String appId : appIds) {
+            for (String profileId : profileIds) {
+                FindCurrentReleaseOrder order = new FindCurrentReleaseOrder();
+                order.setAppId(appId);
+                order.setProfileId(profileId);
+
+                FindCurrentReleaseResult result = releaseService.findCurrentRelease(order);
+                checkResult(result, Status.SUCCESS);
+            }
+        }
+    }
+
+    @Test
+    private void testQueryReleases() {
+        String[] appIds = new String[]{"common", "core-domain", "account", "customer"};
+        String[] profileIds = new String[]{"offline", "dev"};
+        for (String appId : appIds) {
+            for (String profileId : profileIds) {
+                QueryReleasesOrder order = new QueryReleasesOrder();
+                order.setPageNo(1);
+                order.setPageSize(10);
+                order.setAppId(appId);
+                order.setProfileId(profileId);
+
+                QueryReleasesResult result = releaseService.queryReleases(order);
+                checkResult(result, Status.SUCCESS);
+            }
+        }
+    }
+}
