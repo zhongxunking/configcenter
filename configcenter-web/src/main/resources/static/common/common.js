3e1605d2189e31debf76bbf9f307aae73f68ade9
zhongxunking <zhongxunking@163.com>
2019-02-21 22:17:28 +0800

    发布历史页面：显示发布时间

--- configcenter-web/src/main/resources/static/common/common.js
+++ configcenter-web/src/main/resources/static/common/common.js
@@ -1,2 +1,18 @@
 // 设置登录页路径
 MANAGER_LOGIN_PATH = 'login.html';
+// 时间格式化
+Date.prototype.format = function (fmt) { //author: meizz
+    let o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (let k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
