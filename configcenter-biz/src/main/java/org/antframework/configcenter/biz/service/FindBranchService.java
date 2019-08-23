9ffd1f77e28b0fd3258702767b85a6cdbdda2106
zhongxunking <zhongxunking@163.com>
2019-08-23 21:22:32 +0800

    查找分支时校验应用、环境是否存在

--- configcenter-biz/src/main/java/org/antframework/configcenter/biz/service/FindBranchService.java
+++ configcenter-biz/src/main/java/org/antframework/configcenter/biz/service/FindBranchService.java
@@ -9,13 +9,21 @@
 package org.antframework.configcenter.biz.service;
 
 import lombok.AllArgsConstructor;
+import org.antframework.common.util.facade.BizException;
+import org.antframework.common.util.facade.CommonResultCode;
 import org.antframework.common.util.facade.FacadeUtils;
+import org.antframework.common.util.facade.Status;
+import org.antframework.configcenter.biz.util.Apps;
+import org.antframework.configcenter.biz.util.Profiles;
 import org.antframework.configcenter.dal.dao.BranchDao;
 import org.antframework.configcenter.dal.entity.Branch;
+import org.antframework.configcenter.facade.info.AppInfo;
 import org.antframework.configcenter.facade.info.BranchInfo;
+import org.antframework.configcenter.facade.info.ProfileInfo;
 import org.antframework.configcenter.facade.order.FindBranchOrder;
 import org.antframework.configcenter.facade.result.FindBranchResult;
 import org.bekit.service.annotation.service.Service;
+import org.bekit.service.annotation.service.ServiceBefore;
 import org.bekit.service.annotation.service.ServiceExecute;
 import org.bekit.service.engine.ServiceContext;
 import org.springframework.core.convert.converter.Converter;
@@ -31,6 +39,20 @@ public class FindBranchService {
 
     // 分支dao
     private final BranchDao branchDao;
+
+    @ServiceBefore
+    public void before(ServiceContext<FindBranchOrder, FindBranchResult> context) {
+        FindBranchOrder order = context.getOrder();
+        // 校验入参
+        AppInfo app = Apps.findApp(order.getAppId());
+        if (app == null) {
+            throw new BizException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("应用[%s]不存在", order.getAppId()));
+        }
+        ProfileInfo profile = Profiles.findProfile(order.getProfileId());
+        if (profile == null) {
+            throw new BizException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("环境[%s]不存在", order.getProfileId()));
+        }
+    }
 
     @ServiceExecute
     public void execute(ServiceContext<FindBranchOrder, FindBranchResult> context) {
