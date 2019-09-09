92a752bfd50aef51955a6ee317bcd004bfa236a4
zhongxunking <zhongxunking@163.com>
2019-09-09 22:54:24 +0800

    回滚分支后，通知客户端刷新配置

--- configcenter-biz/src/main/java/org/antframework/configcenter/biz/service/RevertBranchService.java
+++ configcenter-biz/src/main/java/org/antframework/configcenter/biz/service/RevertBranchService.java
@@ -13,17 +13,17 @@ import org.antframework.common.util.facade.BizException;
 import org.antframework.common.util.facade.CommonResultCode;
 import org.antframework.common.util.facade.EmptyResult;
 import org.antframework.common.util.facade.Status;
-import org.antframework.configcenter.biz.util.Releases;
+import org.antframework.configcenter.biz.util.Refreshes;
 import org.antframework.configcenter.dal.dao.BranchDao;
 import org.antframework.configcenter.dal.dao.MergenceDao;
 import org.antframework.configcenter.dal.dao.ReleaseDao;
 import org.antframework.configcenter.dal.entity.Branch;
 import org.antframework.configcenter.dal.entity.Mergence;
 import org.antframework.configcenter.dal.entity.Release;
-import org.antframework.configcenter.facade.info.ReleaseInfo;
 import org.antframework.configcenter.facade.order.RevertBranchOrder;
 import org.antframework.configcenter.facade.vo.ReleaseConstant;
 import org.bekit.service.annotation.service.Service;
+import org.bekit.service.annotation.service.ServiceAfter;
 import org.bekit.service.annotation.service.ServiceExecute;
 import org.bekit.service.engine.ServiceContext;
 
@@ -53,12 +53,14 @@ public class RevertBranchService {
         if (branch == null) {
             throw new BizException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("分支[appId=%s,profileId=%s,branchId=%s]不存在", order.getAppId(), order.getProfileId(), order.getBranchId()));
         }
-        ReleaseInfo targetRelease = Releases.findRelease(order.getAppId(), order.getProfileId(), order.getTargetReleaseVersion());
-        if (targetRelease == null) {
-            throw new BizException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("回滚到的目标发布[appId=%s,profileId=%s,version=%d]不存在", order.getAppId(), order.getProfileId(), order.getTargetReleaseVersion()));
+        if (order.getTargetReleaseVersion() > ReleaseConstant.ORIGIN_VERSION) {
+            Release targetRelease = releaseDao.findLockByAppIdAndProfileIdAndVersion(order.getAppId(), order.getProfileId(), order.getTargetReleaseVersion());
+            if (targetRelease == null) {
+                throw new BizException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("回滚到的目标发布[appId=%s,profileId=%s,version=%d]不存在", order.getAppId(), order.getProfileId(), order.getTargetReleaseVersion()));
+            }
         }
         // 删除未被关联的发布
-        Set<Long> touchedReleaseVersions = getTouchedReleaseVersions(branch, targetRelease);
+        Set<Long> touchedReleaseVersions = getTouchedReleaseVersions(branch, order.getTargetReleaseVersion());
         deleteDetachedReleases(branch, touchedReleaseVersions);
         // 更新分支
         branch.setReleaseVersion(order.getTargetReleaseVersion());
@@ -66,13 +68,13 @@ public class RevertBranchService {
     }
 
     // 获取被关联的发布的版本
-    private Set<Long> getTouchedReleaseVersions(Branch branch, ReleaseInfo targetRelease) {
+    private Set<Long> getTouchedReleaseVersions(Branch branch, long targetReleaseVersion) {
         List<Branch> branches = branchDao.findLockByAppIdAndProfileId(branch.getAppId(), branch.getProfileId());
         Set<Long> versions = branches.stream()
                 .filter(b -> !Objects.equals(b.getBranchId(), branch.getBranchId()))
                 .map(Branch::getReleaseVersion)
                 .collect(Collectors.toSet());
-        versions.add(targetRelease.getVersion());
+        versions.add(targetReleaseVersion);
         return versions;
     }
 
@@ -80,9 +82,7 @@ public class RevertBranchService {
     private void deleteDetachedReleases(Branch branch, Set<Long> touchedReleaseVersions) {
         long version = branch.getReleaseVersion();
         while (version > ReleaseConstant.ORIGIN_VERSION && !touchedReleaseVersions.contains(version)) {
-            // 删除相关的合并
-            deleteMergences(branch.getAppId(), branch.getProfileId(), version);
-            // 判断删除发布
+            // 判断是否删除发布
             Release release = releaseDao.findLockByAppIdAndProfileIdAndVersion(branch.getAppId(), branch.getProfileId(), version);
             if (release == null) {
                 throw new BizException(Status.FAIL, CommonResultCode.ILLEGAL_STATE.getCode(), String.format("发布[appId=%s,profileId=%s,version=%d]不存在", branch.getAppId(), branch.getProfileId(), version));
@@ -91,6 +91,9 @@ public class RevertBranchService {
             if (existsChildren) {
                 break;
             }
+            // 删除相关的合并
+            deleteMergences(branch.getAppId(), branch.getProfileId(), version);
+            // 删除发布
             releaseDao.delete(release);
             // 切换到父版本
             version = release.getParentVersion();
@@ -105,5 +108,12 @@ public class RevertBranchService {
         }
         List<Mergence> mergences = mergenceDao.findLockByAppIdAndProfileIdAndSourceReleaseVersion(appId, profileId, version);
         mergences.forEach(mergenceDao::delete);
+    }
+
+    @ServiceAfter
+    public void after(ServiceContext<RevertBranchOrder, EmptyResult> context) {
+        RevertBranchOrder order = context.getOrder();
+        // 刷新客户端
+        Refreshes.refreshClients(order.getAppId(), order.getProfileId());
     }
 }
