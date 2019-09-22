4cab972bf72fe8ae849aaaf7b2933cfa8b11c01e
zhongxunking <zhongxunking@163.com>
2019-09-22 22:27:58 +0800

    调整ReleaseServiceTest

--- configcenter-test/src/test/java/org/antframework/configcenter/test/facade/api/ReleaseServiceTest.java
+++ configcenter-test/src/test/java/org/antframework/configcenter/test/facade/api/ReleaseServiceTest.java
@@ -8,12 +8,13 @@
  */
 package org.antframework.configcenter.test.facade.api;
 
-import org.antframework.common.util.facade.Status;
+import org.antframework.common.util.facade.FacadeUtils;
 import org.antframework.configcenter.facade.api.ReleaseService;
 import org.antframework.configcenter.facade.order.FindReleaseOrder;
 import org.antframework.configcenter.facade.order.QueryReleasesOrder;
 import org.antframework.configcenter.facade.result.FindReleaseResult;
 import org.antframework.configcenter.facade.result.QueryReleasesResult;
+import org.antframework.configcenter.facade.vo.ReleaseConstant;
 import org.antframework.configcenter.test.AbstractTest;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -24,40 +25,32 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @Ignore
 public class ReleaseServiceTest extends AbstractTest {
-    private static final String[] APP_IDS = new String[]{"common", "core-domain", "account", "customer"};
-    private static final String[] PROFILE_IDS = new String[]{"offline", "dev"};
-
     @Autowired
     private ReleaseService releaseService;
 
     @Test
     public void testFindRelease() {
-        for (String appId : APP_IDS) {
-            for (String profileId : PROFILE_IDS) {
-                FindReleaseOrder order = new FindReleaseOrder();
-                order.setAppId(appId);
-                order.setProfileId(profileId);
-                order.setVersion(3L);
+        FindReleaseOrder order = new FindReleaseOrder();
+        order.setAppId("customer");
+        order.setProfileId("dev");
+        order.setVersion(ReleaseConstant.ORIGIN_VERSION);
 
-                FindReleaseResult result = releaseService.findRelease(order);
-                checkResult(result, Status.SUCCESS);
-            }
-        }
+        FindReleaseResult result = releaseService.findRelease(order);
+        FacadeUtils.assertSuccess(result);
     }
 
     @Test
     public void testQueryReleases() {
-        for (String appId : APP_IDS) {
-            for (String profileId : PROFILE_IDS) {
-                QueryReleasesOrder order = new QueryReleasesOrder();
-                order.setPageNo(1);
-                order.setPageSize(10);
-                order.setAppId(appId);
-                order.setProfileId(profileId);
+        QueryReleasesOrder order = new QueryReleasesOrder();
+        order.setPageNo(1);
+        order.setPageSize(10);
+        order.setAppId(null);
+        order.setProfileId(null);
+        order.setVersion(null);
+        order.setMemo(null);
+        order.setParentVersion(null);
 
-                QueryReleasesResult result = releaseService.queryReleases(order);
-                checkResult(result, Status.SUCCESS);
-            }
-        }
+        QueryReleasesResult result = releaseService.queryReleases(order);
+        FacadeUtils.assertSuccess(result);
     }
 }
