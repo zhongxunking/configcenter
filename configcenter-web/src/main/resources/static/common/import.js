05cf8846ae478bfa6e5615bfc36dbf8bd1cabdaa
zhongxunking <zhongxunking@163.com>
2021-04-12 21:50:22 +0800

    js资源版本化

--- configcenter-web/src/main/resources/static/common/import.js
+++ configcenter-web/src/main/resources/static/common/import.js
@@ -1,3 +1,4 @@
+CONFIGCENTER_VERSION = '1.7.0';
 document.write("<script src=\"../manager/import.js?version=" + CONFIGCENTER_VERSION + "\"><\/script>");
 document.write("<script src=\"../common/common.js?version=" + CONFIGCENTER_VERSION + "\"><\/script>");
 document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"../lib/icon/iconfont.css?version=" + CONFIGCENTER_VERSION + "\">");
