f6d9746cdd0b37fb0d9a9cfdc74883cd61ec9673
zhongxunking <zhongxunking@163.com>
2021-04-18 22:21:28 +0800

    升级版本到1.7.1

--- configcenter-web/src/main/resources/static/common/import.js
+++ configcenter-web/src/main/resources/static/common/import.js
@@ -1,4 +1,4 @@
-CONFIGCENTER_VERSION = '1.7.0';
+CONFIGCENTER_VERSION = '1.7.1';
 document.write("<script src=\"../manager/import.js?version=" + CONFIGCENTER_VERSION + "\"><\/script>");
 document.write("<script src=\"../common/common.js?version=" + CONFIGCENTER_VERSION + "\"><\/script>");
 document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"../lib/icon/iconfont.css?version=" + CONFIGCENTER_VERSION + "\">");
